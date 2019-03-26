7c240ff44650238d1b7c1cdae48d8d6ddf731cc2
Chirurgus <sasha.sashaxl@gmail.com>
2019-03-26 20:59:17 +0100

    Change stock_history method name

--- finance.py
+++ finance.py
@@ -3,9 +3,9 @@
 
 from random import sample
 
-from iexdata import symbols, stock_history
+from iexdata import symbols, stock_history_daily
 
 if __name__ == "__main__":
     all_symbols = symbols()
-    history = stock_history(sample(all_symbols, 100))
+    history = stock_history_daily(sample(all_symbols, 100))
     history.to_csv(r'stock_history.csv')
