e4614e7ec66b58083e6e9fcda08dac5d5676c776
Chirurgus <sasha.sashaxl@gmail.com>
2019-03-26 21:01:12 +0100

    Update tests

--- iexdata/test.py
+++ iexdata/test.py
@@ -5,7 +5,7 @@ from unittest import TestCase
 
 import pandas as pd
 
-from . import symbols, stock_history
+from . import symbols, stock_history_daily
 
 class TestStockPriceHistory(TestCase):
     symbols = ["AAPL", "TSLA"]
@@ -17,14 +17,14 @@ class TestStockPriceHistory(TestCase):
         self.assertIsInstance(all_symbols, list)
     
     def test_stock_history_type(self):
-        history = stock_history(self.symbols)
+        history = stock_history_daily(self.symbols)
         self.assertIsInstance(history, pd.DataFrame)
 
     def test_stock_history_col_count(self):
-        history = stock_history(self.symbols)
+        history = stock_history_daily(self.symbols)
         self.assertEqual(len(history.columns), self.expected_num_col)
 
     def test_stock_history_row_count(self):
-        history = stock_history(self.symbols)
+        history = stock_history_daily(self.symbols)
         self.assertEqual(len(history.index),
                          self.expected_num_row_per_symbol*len(self.symbols))
